356a890fedec3a29494f31ff06c822779b92d08d
parikhparam28 <parikhparam28@gmail.com>
2022-06-10 17:03:08 +0530

    dependencies error removed

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -75,7 +75,7 @@ const Chart = ({data: {confirmed , deaths , recovered} , country}) => {
               labels: ['Infected', 'Recovered', 'Deaths'],
               datasets: [
                 {
-                  label: ['Infected', 'Recovered', 'Deaths'],
+                  label: ['People'],
                   backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
                   data: [confirmed.value, recovered.value, deaths.value],
                 },
